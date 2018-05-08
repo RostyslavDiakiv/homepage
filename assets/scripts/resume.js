58e105048ead428c45a286e1e6015ef00452f670
Diakiv Rostyslav <rostyslav.diakiv@gmail.com>
2018-05-08 21:33:56 +0300

    Added favico, cleanup code, fixed markup and styles, added comments, renamed classes

--- assets/scripts/resume.js
+++ assets/scripts/resume.js
@@ -13,18 +13,14 @@ const obj = {
     }
 };
 
-// const bar = new ProgressBar.Line(container, obj);
-
 const mssqlserverf = new ProgressBar.Line(mssqlserver, obj);
 const csharpf = new ProgressBar.Line(csharp, obj);
 const javaf = new ProgressBar.Line(java, obj);
 const frontendf = new ProgressBar.Line(frontend, obj);
 const algorithmsf = new ProgressBar.Line(algorithms, obj);
 
-//bar.animate(0.4);
-
-mssqlserverf.animate(0.6);
-csharpf.animate(1.0);  // Number from 0.0 to 1.0
+mssqlserverf.animate(0.6); // Number from 0.0 to 1.0
+csharpf.animate(1.0);
 javaf.animate(0.3);
 frontendf.animate(0.7);
 algorithmsf.animate(0.9);
